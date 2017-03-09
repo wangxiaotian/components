4b4df26cc38e522d78dafd9b6cdf38a6855a73a9
[kaiyang] <634874196@qq.com>
2017-03-09 15:36:21 +0800

    a

--- table/table.js
+++ table/table.js
@@ -7,7 +7,17 @@
 	var XXtable = function(){};
 	XXtable.prototype.configuration = {
 		container: '',
-		url: ''
+		url: '',
+		thRow: [{
+			'name': 'name',
+			'alias': '姓名',
+		},{
+			'name': 'job',
+			'alias': '工作'
+		},{
+			'name': 'phone',
+			'alisa': '电话'
+		}]
 	};
 	XXtable.prototype.init = function(options){
 		var self = this;
@@ -15,6 +25,8 @@
 		$.extend(true,this,self.configuration,options);
 		//	启动
 		self.initFrame();
+		//	初次渲染数据
+		self.getData();
 	};
 	//	初始化组件结构
 	XXtable.prototype.initFrame = function(){
@@ -24,11 +36,39 @@
 						<div class = "table-footer"></div>\
 					</div>';
 		self.$container.append($(str));
+		self.tableContainer = $('.XX-table').find('table-body');
+		self.tableFooter = $('.XX-table').find('table-footer');
 	};
+	//	请求数据
+	XXtable.prototype.getData = function(){
+		var self = this;
+		var url = self.url,
+			data = '';
+		self.thData = [];
+		$.each(self.thRow,function(index,value){
+			self.thData.push(value.alias);
+		})
+		Utils.getData(url,data,function(res){
+			res.data.thData = self.thData;
+			self.render(res.data);
+		})
+
+	};
+	//	渲染
+	XXtable.prototype.render = function(data){
+		var self = this;
+		Utils.requireTpl('table',function(tpl){
+			Utils.render({
+				tpl: tpl,
+				data: data,
+				container: self.tableContainer
+			})
+		})
+	}
 	window.xxtable = {
 		init: function(options){
-			var xxtable = new XXtable(options);
-			return xxtable;
+			var xxtable = new XXtable();
+			xxtable.init(options);
 		}
 	}
 })()
