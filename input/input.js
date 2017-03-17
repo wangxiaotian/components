c5bec7c7891f507c7d54846b68a86effeafafc5e
[kaiyang] <634874196@qq.com>
2017-03-17 18:49:19 +0800

    save

--- input/input.js
+++ input/input.js
@@ -1,23 +1,23 @@
 ;
 (function() {
     var XXinput = {};
-    //	文本输入框
+    //  文本输入框
     (function() {
         var Text = XXinput.Text = function() {};
         Text.prototype.configuration = {
-            //	容器
+            //  容器
             container: '',
-            //	name值
+            //  name值
             name: '',
-            //	label名
+            //  label名
             alias: '',
-            //	占位值
+            //  占位值
             placeholder: '',
-            //	后缀
+            //  后缀
             postfix: '',
-            //	输入框长度
+            //  输入框长度
             length: '',
-            //	label长度
+            //  label长度
             labelLength: '',
             // 可读
             readonly: false
@@ -38,24 +38,24 @@
         };
         Text.prototype.initFrame = function() {
             var self = this;
-            //	水平表单带后缀
+            //  水平表单带后缀
             var _htmlHorizontalWithPostfix = '<div class="form-group">\
-												<label for="" class="col-xs-' + self.labelLength + ' control-label">' + self.alias + '</label>\
-												<div class="col-xs-' + self.length + '">\
-													<div class="input-group">\
-														<input type="text" name = "' + self.name + '" class="form-control" placeholder = "' + self.placeholder + '">\
-														<span class="input-group-addon">' + self.postfix + '</span>\
-													</div>\
-												</div>\
-											</div>';
+                                                <label for="" class="col-xs-' + self.labelLength + ' control-label">' + self.alias + '</label>\
+                                                <div class="col-xs-' + self.length + '">\
+                                                    <div class="input-group">\
+                                                        <input type="text" name = "' + self.name + '" class="form-control" placeholder = "' + self.placeholder + '">\
+                                                        <span class="input-group-addon">' + self.postfix + '</span>\
+                                                    </div>\
+                                                </div>\
+                                            </div>';
             var _htmlHorizontal = '<div class="form-group">\
-										<label for="" class="col-xs-' + self.labelLength + ' control-label">' + self.alias + '</label>\
-										<div class="col-xs-' + self.length + '">\
-											<input type="text" name = "' + self.name + '" class="form-control" placeholder = "' + self.placeholder + '">\
-										</div>\
-									</div>';
+                                        <label for="" class="col-xs-' + self.labelLength + ' control-label">' + self.alias + '</label>\
+                                        <div class="col-xs-' + self.length + '">\
+                                            <input type="text" name = "' + self.name + '" class="form-control" placeholder = "' + self.placeholder + '">\
+                                        </div>\
+                                    </div>';
             var _html = '';
-            //	判断是否有后缀
+            //  判断是否有后缀
             if (!!self.postfix) {
                 _html = _htmlHorizontalWithPostfix;
 
@@ -64,35 +64,35 @@
             }
             var $input = $(_html);
             self.$container.replaceWith($input);
-            //	添加可读性
+            //  添加可读性
             if (self.readonly) {
                 $input.find('input').attr('readonly', 'readonly');
             }
         }
     })();
-    //	下拉选择框
+    //  下拉选择框
     (function() {
         var Select = XXinput.Select = function() {};
         Select.prototype.configuration = {
-            //	容器
+            //  容器
             container: '',
-            //	数据源
+            //  数据源
             dataSource: {
                 url: '',
                 params: {}
             },
-            //	label长度
+            //  label长度
             labelLength: '',
-            //	选择框长度
+            //  选择框长度
             length: '',
-            //	label name
+            //  label name
             alias: '',
-            //	默认项
+            //  默认项
             defaultOption: '',
-            dataCallback: function(res){
-            	console.log('后台返回数据格式不能直接使用，需要自己配置数据格式并返回');
-            	var data = res.data;
-            	return data;
+            dataCallback: function(res) {
+                console.log('后台返回数据格式不能直接使用，需要自己配置数据格式并返回');
+                var data = res.data;
+                return data;
             }
         };
         Select.prototype.init = function(options) {
@@ -122,11 +122,11 @@
             self.$container.replaceWith(_selectHtml);
             self.moreOption = _selectHtml.find('.more-option')
         };
-        //	获取数据
+        //  获取数据
         Select.prototype.getData = function() {
             var self = this;
             var _moreOption;
-                //	dataSource Array
+            //  dataSource Array
             if (self.dataSource.params instanceof Array) {
                 console.log('暂不考虑数组情况');
                 return;
@@ -139,7 +139,7 @@
                 })
             }
         };
-        //	渲染
+        //  渲染
         Select.prototype.render = function(data) {
             var self = this;
             Utils.requireTpl('select', function(tpl) {
@@ -151,16 +151,75 @@
             })
         };
     })();
+    //  表单
+    (function() {
+        var Form = XXinput.Form = function() {};
+        Form.prototype.configuration = {
+            //  容器
+            container: '',
+            //  表单提交地址
+            action: '',
+            //  提交按钮
+            submitBtn: ''
+        };
+        Form.prototype.init = function(options) {
+            var self = this;
+            $.extend(true, this, self.configuration, options);
+            self.initStatus();
+            self.submit();
+        };
+        Form.prototype.initStatus = function() {
+            var self = this;
+            self.$container = $(self.container);
+            self.submitBtn = $(self.submitBtn);
+        };
+        Form.prototype.getVal = function() {
+            var self = this;
+            var paramsStr = self.$container.serialize();
+            self.postData = Utils.paramsString2obj(paramsStr);
+        };
+        Form.prototype.submit = function() {
+            var self = this;
+            //  提交
+            self.submitBtn.on('click', function(event) {
+                //  获取文本值
+                self.getVal();
+                console.log(typeof self.postData)
+                Utils.postData(self.action, self.postData, function(res) {
+                    if (res.code == 0) {
+                        xxpopup.init({
+                            container: '#myModal-alert',
+                            title: '提示',
+                            body: '提交成功！',
+                            type: 'dialog'
+                        })
+                    } else {
+                        xxpopup.init({
+                            container: '#myModal-alert',
+                            title: '提示',
+                            body: '提交失败！',
+                            type: 'alert'
+                        })
+                    }
+                })
+            })
+        }
+    })()
     window.xxinput = {
         initText: function(options) {
             var text = new XXinput.Text();
             text.init(options);
             return text;
         },
-        initSelect: function(options){
-        	var select = new XXinput.Select();
-        	select.init(options);
-        	return select;
+        initSelect: function(options) {
+            var select = new XXinput.Select();
+            select.init(options);
+            return select;
+        },
+        initForm: function(options) {
+            var form = new XXinput.Form();
+            form.init(options);
+            return form;
         }
     }
 })()
